59de0b86b5fd7f1d8c97e13aa74b725ade1efaae
Ritvik25goyal <goyalritvik9@gmail.com>
2025-03-20 16:33:43 +0530

    Updated lab6

---
+++ lab6/lab6_ex1.m
@@ -0,0 +1,106 @@
+clc;
+clearvars;
+%  We are solv1ng a 2 po1nt BVP problem w1th h3lp of finite difference  scheme
+% given problem
+% u'' = -u + f   -1<t<1
+% u(-1) = u(1) = 0
+% where f is a some function of t , u , u'
+
+% Initial values according to the problem
+a=-1;
+b=1;
+alpha = 0;
+beta = 0;
+% df/dy and df/d(y')
+dfdy = @(t,y,dy) 0;
+dfddy = @(t,y,dy) 1;
+% f(t,u,u') Consider it as a vector function
+f = @(t,y,y1)  t + y1;
+
+M = 10;
+error = zeros(1:M);
+
+for po = 3:M
+    % Number of steps
+    N = 2^po;
+    printf("Current N: %d",N);
+
+    % Step size
+    h = (b-a)/(N+1);
+
+    % A matrix tridiagonal
+    A = spdiags(ones(N,1)*[1 -2 1], -1:1, N, N);
+
+    % Exact Solution
+    yexact = zeros(1,N);
+    co1 = 2/(exp(1) - exp(-1));
+    co2 = (-1/2) -co1*(exp(-1));
+    for i = 2:N+1
+        t = -1 + (i-1)*h;
+        yexact(i-1) = -((t^2)/2) - t + (co1*exp(t)) + co2;
+    end
+
+    % Initialise values;
+    t = a + h*([1:N]');
+    y = zeros(N,1);
+    dy = zeros(N,1);
+    ddy = zeros(N,1);
+    g = zeros(N,1);
+    g(1) = -alpha/(h*h);
+    g(end) = -beta/(h*h);
+
+    %  Initialized y with line interpolation
+    for i = 1:N
+        y(i) = 1;
+    end
+
+    while (1)
+
+        % y' Calculation
+        dy(2:end-1) = (y(3:end) - y(1:end-2)) / (2*h);
+        % Handle boundaries
+        dy(1) = (y(2) - alpha) / (2*h);
+        dy(end) = (beta - y(end-1)) / (2*h);
+
+        % F vector
+        fvec = f(t,y,dy);
+
+        % We are solving Ay/(h*h) - fvec - g = 0
+        H = (A*y)./(h*h) - fvec - g ;
+
+        % Calculating F jacobian
+        Fj = zeros(N,N);
+        for i = 1:N
+            Fj(i,i) = dfdy(a+i*h,y(i),dy(i));
+        end
+        for i = 1:N-1
+            Fj(i,i+1) = dfddy(a+i*h,y(i),dy(i))/(2*h);
+            Fj(i+1,i) = -dfddy(a+i*h,y(i+1),dy(i+1))/(2*h);
+        end
+        dH = A/(h*h) - Fj;
+
+        % Newtons step
+        y1 = y - (dH\H);
+
+        % Epsilon check
+        if (abs(norm(y1-y))<1e-8)
+              y = y1;
+              disp(y);
+            break;
+        end
+        y = y1;
+    end
+
+    error(po) = max(abs(yexact-y'))/max(abs(yexact));
+end
+
+fprintf('\n');
+for m = 3:M
+    fprintf('%6d \t %0.6e \t %0.2f\n', 2^m, error(m), error(m-1)/error(m));
+end
+
+% Ploting
+plot(a+[1:N]*h,yexact,'LineWidth',2);
+hold on;
+plot(a+[1:N]*h,y,'--','LineWidth',2);
+legend('yexact','y');
